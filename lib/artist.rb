6db21d60944099834e2c56a00503901aeb34abe2
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-19 15:24:56 -0800

    all done

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize
+        @name
+    end
+
+    def self.all
+        @@all
+    end
+    
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+
+class Song
+
+    attr_accessor :title, :artist
+
+    def initialize
+        @title
+        @artist
+    end
+
+
+end
